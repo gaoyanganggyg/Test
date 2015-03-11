7872b2a24fe2b5473b91b887fb3a23a86c0c0b8d
Gao Yangang <gaoyangangde@163.com>
2015-03-11 21:33:06 +0800

    7

--- Test/src/com/gyg/test/Test.java
+++ Test/src/com/gyg/test/Test.java
@@ -1,10 +1,14 @@
 package com.gyg.test;
 
+import org.apache.log4j.Logger;
+
 public class Test {
 	public void hello() {
 		System.out.println("gao");
 	}
 
 	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Test.class);
+		logger.info("gaoyangang");
 	}
 }
