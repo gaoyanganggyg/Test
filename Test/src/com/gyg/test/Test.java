ba2245ec60a63c89cb535bb70f949a6d58ccf8df
Gao Yangang <gaoyangangde@163.com>
2015-03-12 22:45:20 +0800

    Test11 add

--- Test/src/com/gyg/test/Test.java
+++ Test/src/com/gyg/test/Test.java
@@ -2,13 +2,35 @@ package com.gyg.test;
 
 import org.apache.log4j.Logger;
 
-public class Test {
-	public void hello() {
-		System.out.println("gao");
+public class Test  extends Test11 {
+	public  Animal hello() {
+		return new Animal() {
+			void eat() {
+				System.out.println("eat");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(Test.class);
-		logger.info("gaoyangang");
+		Test test = new Test();
+		test.hello().eat();
+		test.sayHello();
+		test.hello().sleep();
 	}
 }
+abstract class Animal{
+	abstract void eat();
+	void sleep(){
+		System.out.println("sleep");
+	}
+}
+
+class Test11{
+	public void sayHello(){
+		System.out.println("say Hello");
+	}
+}
+
+
+
