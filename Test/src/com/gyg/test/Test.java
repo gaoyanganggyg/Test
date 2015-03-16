99051679e6aac8df213ba419cc82649ef0b06609
Gao Yangang <gaoyangangde@163.com>
2015-03-16 12:11:26 +0800

    1st version

--- Test/src/com/gyg/test/Test.java
+++ Test/src/com/gyg/test/Test.java
@@ -2,21 +2,32 @@ package com.gyg.test;
 
 import org.apache.log4j.Logger;
 
-public class Test  extends Test11 {
-	public  Animal hello() {
-		return new Animal() {
-			void eat() {
-				System.out.println("eat");
-			}
-		};
+/**
+ * 
+ * @author root
+ *@see Test15
+ */
+public  class Test  extends Test11 {
+	int a = 10;
+	public void test() {
+		super.name ++;
+		System.out.println( "this.name    " + this.name);
+		System.out.println("super.name  " + super.name);
 	}
-
+	 
+	public Test(){
+		System.out.println("Test");
+	}
 	public static void main(String[] args) {
-		Logger logger = Logger.getLogger(Test.class);
+		 Logger logger = Logger.getLogger(Test.class);
 		Test test = new Test();
-		test.hello().eat();
-		test.sayHello();
-		test.hello().sleep();
+		test.new Test12();
+		
+		//test.test();
+		
+//		test.hello().eat();
+//		test.sayHello();
+//		test.hello().sleep();
 	}
 }
 abstract class Animal{
@@ -27,10 +38,35 @@ abstract class Animal{
 }
 
 class Test11{
+	public Test11() {
+		System.out.println("Test11");
+	}
+	class Test12{
+		 public Test12() {
+			 System.out.println("Test12");
+		 }
+	 }
+	public  Animal hello() {
+		return new Animal() {
+			void eat() {
+				System.out.println("eat");
+			}
+		};
+	}
+	protected int name = 3;
 	public void sayHello(){
 		System.out.println("say Hello");
 	}
 }
+
+// class Test13 extends Test.Test12{
+//public Test13(Test test) {
+//	test.
+//}}
+
+
+
+
 
 
 
---
+++ Test/src/com/gyg/test/Test15.java
@@ -0,0 +1,50 @@
+package com.gyg.test;
+
+import java.security.Permission;
+import java.util.AbstractCollection;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test15 {
+	public static void main(String[] args) {
+//		List<Integer> list = Arrays.asList(2,1,3);
+//		Collections.sort(list);
+//		Collections.shuffle(list);
+//		Iterator<Integer> iterator = list.iterator();
+//		while(iterator.hasNext()){
+//			System.out.println(iterator.next());
+//		}
+		int[] i = new int[1];
+		System.out.println(i);
+			
+		for (Map.Entry<String, String> j : System.getenv().entrySet()) {
+			System.out.println(j.getKey() + "  " + j.getValue());
+		}
+Arrays
+		
+		
+		
+		//		System.out.println(list.toString());
+//		for (Integer integer : list) {
+//			System.out.println(integer);
+//		}
+		
+		
+		//Collections.sort(list);
+		//System.out.println(Math.pow(2, 31.0));
+		
+		//Collections.shuffle(list);
+		//System.out.println(System.nanoTime());
+		
+		//System.out.println("");
+		
+	}
+	
+}
