054c04a8c16805af4240d9deb690edfd498af6e5
Gao Yangang <gaoyangangde@163.com>
2015-05-14 21:38:13 +0800

    add Test to GitHub

--- Test/src/com/gyg/test/Test15.java
+++ Test/src/com/gyg/test/Test15.java
@@ -2,8 +2,10 @@ package com.gyg.test;
 
 import java.security.Permission;
 import java.util.AbstractCollection;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Formatter;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -11,9 +13,19 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
 
 public class Test15 {
-	public static void main(String[] args) {
+	
+	static ArrayList<Integer> list;
+
+	public void name() {
+		
+	}
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args)  {
 //		List<Integer> list = Arrays.asList(2,1,3);
 //		Collections.sort(list);
 //		Collections.shuffle(list);
@@ -27,9 +39,33 @@ public class Test15 {
 		for (Map.Entry<String, String> j : System.getenv().entrySet()) {
 			System.out.println(j.getKey() + "  " + j.getValue());
 		}
-Arrays
+
+		try {
+			int a = 2 / 0;
+			int b = a;
+		} catch (Exception e) {
+			System.out.println("a");
+			e.printStackTrace();
+		}
+		
+		list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		
+		if(0 == 2){
+		    throw new StringIndexOutOfBoundsException(0);
+		}
+
+		List<Test15> list = new ArrayList<>();
+		list.add(new Test151());
+		list.get(0).name();
 		
 		
+		
+		System.err.println("error");
+		
+		
+		System.out.println(Long.MAX_VALUE);
 		
 		//		System.out.println(list.toString());
 //		for (Integer integer : list) {
@@ -48,3 +84,8 @@ Arrays
 	}
 	
 }
+class Test151 extends Test15{
+	
+}
+
+
